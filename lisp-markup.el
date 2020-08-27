d6f772d04e0320e5da3de1b6844469523eab7d99
Charles Jackson <cbj170230@utdallas.edu>
2020-08-27 13:58:33 -0500

    Add submode class descriptions with front regexps

--- lisp-markup.el
+++ lisp-markup.el
@@ -15,5 +15,25 @@
   (with-syntax-table html-mode-syntax-table
     (sgml-skip-tag-forward 1))
   0)
+
+;;; add mmm classes
+(mmm-add-classes
+ '((lisp-in-html
+    :submode lisp
+    :face nil
+    :front ",(\\|,@\\|=("
+    :include-front t
+    :back "."
+    :back-offset (lisp-back-matcher))
+   (html-in-lisp
+    :submode html
+    :face nil
+    :front "\\(?1:<\\)[^/=[:space:]]"
+    :front-match 1
+    :include-front t
+    :back "."
+    :back-offset (html-back-matcher))))
+(mmm-add-mode-ext-class 'lisp-mode nil 'html-in-lisp)
+(mmm-add-mode-ext-class 'lisp-mode nil 'lisp-in-html)
 (provide 'lisp-markup-mode)
 ;;; lisp-markup.el ends here
