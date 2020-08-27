3caa262c1eb52c37f75825dd51983c20c096c226
Charles Jackson <cbj170230@utdallas.edu>
2020-08-27 13:58:13 -0500

    Add semantic back matchers

--- lisp-markup.el
+++ lisp-markup.el
@@ -4,5 +4,16 @@
 (require 'lisp-mode)
 (require 'mmm-mode)
 
+(defun lisp-back-matcher ()
+  (forward-char -1)
+  (with-syntax-table lisp-mode-syntax-table
+    (forward-sexp))
+  0)
+
+(defun html-back-matcher ()
+  (forward-char -2)
+  (with-syntax-table html-mode-syntax-table
+    (sgml-skip-tag-forward 1))
+  0)
 (provide 'lisp-markup-mode)
 ;;; lisp-markup.el ends here
