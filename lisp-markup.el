5ae90df340d7b4967ddc6325a092357b646d3869
Charles Jackson <cbj170230@utdallas.edu>
2020-10-06 19:01:27 -0500

    Implement font-lock and indentation

--- lisp-markup.el
+++ lisp-markup.el
@@ -2,107 +2,173 @@
 ;;;; Charles Jackson
 (require 'sgml-mode)
 (require 'lisp-mode)
-(require 'mmm-mode)
 
-;;; add preferences for relevant modes
-(mmm-add-to-major-mode-preferences 'lisp 'lisp-mode t)
-(mmm-add-to-major-mode-preferences 'html 'html-mode t)
-;;; set customs to sensible defaults
-(setf mmm-submode-decoration-level 0)   ;no crazy backgrounds
-(setf mmm-parse-when-idle t)            ;update as buffer changes
-(setq mmm-global-mode 'maybe)           ;turn on mmm-mode in useful buffers
+;;; syntax highlighting
+(font-lock-add-keywords
+ 'lisp-mode
+ ;; keyword tag names
+ '(("</?\\(:[^>/=[:space:]]+\\)" 1 font-lock-builtin-face)
+   ;; regular tag names
+   ("</?\\([^>/=[:space:]]*\\)" 1 font-lock-function-name-face)
+   ;; attribute names
+   ("[[:space:]]\\([-[:alpha:]]+\\)=" 1 font-lock-constant-face)
+   ;; deftag faces
+   ("deftag" . font-lock-keyword-face)
+   ("deftag \\([^ ]+\\) " 1 font-lock-function-name-face)
+   ;; warning about single symbol lisp forms at the end of self closing tags
+   ("=[^[:space:]<>]+[^\") ]\\(/\\)" 1 font-lock-warning-face))
+ 'prepend)
 
-(defun lisp-back-matcher ()
+(font-lock-add-keywords
+ 'lisp-mode
+ '(("&[^ ]+;" . font-lock-builtin-face))
+ 'prepend)
+
+(defmacro with-<>-as-brackets (&optional syntax-table &rest body)
+  (declare (indent 1))
+  (unless syntax-table (setq syntax-table (syntax-table)))
+  `(progn
+     (modify-syntax-entry ?< "(" ,syntax-table)
+     (modify-syntax-entry ?> ")" ,syntax-table)
+     ,@body
+     (modify-syntax-entry ?< "." ,syntax-table)
+     (modify-syntax-entry ?> "." ,syntax-table)))
+
+;;; context
+(defun in-html-p ()
+  (save-excursion
+    (cl-loop
+     with point = (point)
+     for start = (progn (goto-char (or start point))
+                        (when start (forward-char -1))
+                        (html-start-point))
+     while (/= start (point-max))
+     for end = (html-end-point)
+     if (<= start point end)
+     return 
+     (progn
+       (not (cl-loop
+             for lisp-start = (progn (goto-char (or lisp-start point))
+                                (when lisp-start (forward-char -1))
+                                (lisp-start-point))
+             while (/= lisp-start (point-max))
+             for lisp-end = (lisp-end-point)
+             if (<= start lisp-start point lisp-end end)
+             return t))))))
+(defun lisp-start-point ()
   (condition-case nil
-      (progn (forward-char -1)
-             (with-syntax-table lisp-mode-syntax-table
-               (forward-sexp)))
-    (t (goto-char (point-max))))
-    0)
-
-(defun html-back-matcher ()
+      (search-backward-regexp ",(\\|,@\\|=(")
+    (t (point-max))))
+(defun lisp-end-point ()
   (condition-case nil
-      (progn
-        (forward-char -2)
-        (with-syntax-table html-mode-syntax-table
-          (sgml-skip-tag-forward 1)))
-    (t (goto-char (point-max))))
-  0)
-
-;;; add mmm classes
-(mmm-add-classes
- '((lisp-in-html
-    :submode lisp
-    :face nil
-    :front ",(\\|,@\\|=("
-    :include-front t
-    :back "."
-    :back-offset (lisp-back-matcher))
-   (html-in-lisp
-    :submode html
-    :face nil
-    :front "\\(?1:<\\)[^/=[:space:]]"
-    :front-match 1
-    :include-front t
-    :back "."
-    :back-offset (html-back-matcher))))
-(mmm-add-mode-ext-class 'lisp-mode nil 'lisp-in-html)
-(mmm-add-mode-ext-class 'lisp-mode nil 'html-in-lisp)
+      (progn (skip-chars-forward "=,@")
+             (forward-sexp)
+             (point))
+    (t (point-max))))
+(defun html-start-point ()
+  (let ((html-start-re "<[^/=[:space:]()]"))
+    (condition-case nil
+        (if (looking-at-p html-start-re)
+            (point)
+          (let ((spot (cl-loop
+                       while (search-backward "<")
+                       if (looking-at-p html-start-re)
+                       return (point))))
+            (if spot spot (point-max))))
+      (t (point-max)))))
+(defun html-end-point ()
+  (let ((tag-name (save-excursion
+                    (buffer-substring-no-properties
+                     (+ (point) 1) (- (search-forward-regexp "[>/[:space:]]") 1)))))
+    (with-<>-as-brackets lisp-mode-syntax-table
+      (forward-sexp 1)
+      (if (looking-back "/>")
+          (point)
+          (condition-case nil
+              (progn
+                (search-forward (concat "</" tag-name ">"))
+                (point))
+            (t (point-max)))))
+    (point)))
 
 ;;; indentation
 (defun lisp-html-indent-line ()
-  "Indent a line of lisp or html.
-Only works in when mmm-mode is active."
+  "Indent a line of lisp or html."
   (interactive)
-  (mmm-update-current-submode)
-  (if (not mmm-current-submode)
-      (lisp-indent-line)
   (save-excursion
-    (save-restriction
-    (widen)
     (back-to-indentation)
-    (cl-flet ((lisp-html-submode
-               () (let ((ovl (mmm-overlay-at (point))))
-                    (or (and ovl (overlay-get ovl 'mmm-mode))
-                      'lisp-mode)))
-              (special-lisp-html-indent-line
+    (cl-flet ((special-lisp-html-indent-line
                () (let ((prev-exp-ident
                          (save-excursion
-                           (funcall (if (equal prev-mm 'lisp-mode)
+                           (funcall (if (not prev-html)
                                         #'backward-sexp
                                       #'sgml-skip-tag-backward)
                                     1)
                            (skip-chars-backward ",@")
+                           (when (not prev-html)
+                             (while (and (not (in-html-p))
+                                         (/= (char-before) 40))
+                               (backward-sexp))
+                             (when (not (in-html-p)) (forward-sexp))
+                             (skip-chars-forward "\t\r\n "))
                            (- (point) (progn (beginning-of-line) (point))))))
                     (indent-line-to prev-exp-ident))))
-      (let ((prev-mm (save-excursion
-                       (forward-line -1)
-                       (end-of-line)
-                       (lisp-html-submode)))
-            (beg-line-mm (save-excursion
-                           (beginning-of-line)
-                           (lisp-html-submode)))
-            (curr-mm (lisp-html-submode))
-            (closing-p (looking-at "</")))
+      (let ((prev-html (save-excursion
+                         (forward-line -1)
+                         (end-of-line)
+                         (in-html-p)))
+            (beg-line-html (save-excursion
+                             (beginning-of-line)
+                             (in-html-p)))
+            (curr-html (in-html-p)))
         (cond
+         ;; closing tag
+         ((looking-at "</")
+          (let* ((tag-name (save-excursion
+                             (buffer-substring-no-properties
+                              (+ (point) 2)
+                              (progn (search-forward ">")
+                                     (- (point) 1)))))
+                 (indent
+                  (save-excursion
+                    (search-backward-regexp (concat "<" tag-name "[ />]"))
+                    (- (point) (progn (beginning-of-line) (point))))))
+            (message "closing")
+            (indent-line-to (max 0 indent))))
+         ((and prev-html
+               (save-excursion
+                 (forward-line -1)
+                 (back-to-indentation)
+                 (looking-at "</")))
+          (message "odd")
+          (indent-line-to
+           (save-excursion
+             (forward-line -1)
+             (back-to-indentation)
+             (- (point) (progn (beginning-of-line) (point))))))
          ;; special indent
-         ((and (equal prev-mm curr-mm)
-               (not (equal curr-mm beg-line-mm)))
-          (special-lisp-html-indent-line))
+         ;; ((and (eq prev-html curr-html)
+         ;;       (not (eq curr-html beg-line-html)))
+         ;;  (special-lisp-html-indent-line))
          ;; sgml indent
-         ((or (and (equal prev-mm 'html-mode)
-                   (equal curr-mm 'html-mode))
-              (and (equal prev-mm 'html-mode)
-                   (equal curr-mm 'lisp-mode))
-              (and (equal prev-mm 'lisp-mode)
-                   closing-p))
-          (with-syntax-table html-mode-syntax-table
-            (sgml-indent-line)))
+         (prev-html
+          (message "html")
+          ;; (with-<>-as-brackets lisp-mode-syntax-table
+          ;; (sgml-indent-line))
+          (progn
+            (modify-syntax-entry 60 "(" lisp-mode-syntax-table)
+            (modify-syntax-entry 62 ")" lisp-mode-syntax-table)
+            (sgml-indent-line)
+            (modify-syntax-entry 60 "." lisp-mode-syntax-table)
+            (modify-syntax-entry 62 "." lisp-mode-syntax-table)))
          ;; lisp indent
-         (:else (with-syntax-table lisp-mode-syntax-table
-                  (let ((indent (calculate-lisp-indent)))
-                    (cond ((listp indent) (special-lisp-html-indent-line)) ;special indent if lisp-indent is confused
-                          (indent (indent-line-to indent))))))))))))
+         (:else
+          (let ((indent (calculate-lisp-indent)))
+            (message "lisp")
+            (cond
+             ((and indent (listp indent)) (indent-line-to (car indent)))
+             (indent (indent-line-to indent))
+             (:else (special-lisp-html-indent-line)))))))))
   (when (< (point) (save-excursion (back-to-indentation) (point)))
     (back-to-indentation)))
 (defun lisp-html-indent-region (beg end)
@@ -120,11 +186,9 @@ Just calls `lisp-html-indent-line' on every line of the region."
 (defun set-lisp-html-indentation ()
   (setq indent-line-function #'lisp-html-indent-line)
   (setq indent-region-function #'lisp-html-indent-region))
-(add-hook 'mmm-lisp-in-html-class-hook #'set-lisp-html-indentation)
-(add-hook 'mmm-html-in-lisp-class-hook #'set-lisp-html-indentation)
+(add-hook 'lisp-mode-hook #'set-lisp-html-indentation)
 ;; define enter to indent
 (define-key lisp-mode-map (kbd "<return>") #'newline-and-indent)
-(define-key html-mode-map (kbd "<return>") #'newline-and-indent)
 
 (provide 'lisp-markup)
 ;;; lisp-markup.el ends here
